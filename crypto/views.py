88fa4353226c494c15cbbb4dac77d95565abf4ed
Lior Hirschfeld <lhirschfeld@andover.edu>
2017-09-17 09:02:35 -0400

    added charts and modified text

--- crypto/views.py
+++ crypto/views.py
@@ -7,7 +7,7 @@ import time
 def index(request):
     ethValues = []
     spd = 60*60*24
-    url = "https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&limit=90&aggregate=1&e=CCCAGG"
+    url = "https://min-api.cryptocompare.com/data/histohour?fsym=ETH&tsym=USD&limit=168&aggregate=1&e=CCCAGG"
     myResponse = requests.get(url, verify=True)
     if(myResponse.ok):
         jData = json.loads(str(myResponse.content)[2:-1])["Data"]
@@ -15,7 +15,16 @@ def index(request):
             ethValue = 0
             ethValue = jData[i]["close"]
             ethValues.append(ethValue)
+
+    sentiment = 0.610913
+    predictedValue = 5393.6*sentiment - 2957.3
+    if predictedValue > ethValues[len(ethValues)-1]:
+        prediction = 1
+    else:
+        prediction = 0
     c = {
         'ethValues': ethValues,
+        'prediction': prediction,
+        'predictedValue': int(predictedValue),
     }
     return HttpResponse(loader.get_template('crypto/index.html').render(c))
