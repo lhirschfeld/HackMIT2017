a1f699bbfceeb054fc389d975b8f70834cfbabdb
Lior Hirschfeld <lhirschfeld@andover.edu>
2017-10-02 14:57:06 -0400

    updated

--- techSurfers/wsgi.py
+++ techSurfers/wsgi.py
@@ -7,11 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techSurfers.settings")
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techSurfers.settings")
+
 application = get_wsgi_application()
-#
